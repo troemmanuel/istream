ba8595eacc0f551afaf79dd33db6f562003076a7
Emmanuel Kope Tro <trohkopeemmanuel@gmail.com>
2020-09-28 10:12:17 +0000

    UserAdmin fonction basic reussi avec succes.

--- istreamExpress/backend/controllers/fsUserController.js
+++ istreamExpress/backend/controllers/fsUserController.js
@@ -10,7 +10,7 @@ class userAdminControlleur {
      * @param {*} res
      */
     //Afficher
-    fsUserAdminDisplay = (req, res) => {
+    fsUserList = (req, res) => {
 
         let sql = "SELECT * FROM fs_user";
         let query = connexion.query(sql, (err, rows) => {
@@ -23,7 +23,7 @@ class userAdminControlleur {
     }
 
     // Insert datta in fs user table 
-    insertDataInFsuserTable = (req, res) => {
+    fsUserAdd = (req, res) => {
 
         let data = {
             _USER_LEGAL_NAME_: req.body.legalName,
@@ -40,6 +40,56 @@ class userAdminControlleur {
             res.redirect('/')
         });
     }
+    fsUserDelete = (req, res) => {
+
+            const id = req.params.id;
+            let sql = `DELETE from fs_user where USER_ID = ${id}`;
+            let query = connexion.query(sql, (err, result) => {
+                if (err) throw err;
+                res.redirect('/');
+
+            })
+        }
+        // Update data function
+    fsUserEdit = (req, res) => {
+
+        const id = req.params.id;
+        let sql = `SELECT * FROM fs_user WHERE USER_ID = ${id}`;
+        let query = connexion.query(sql, (err, result) => {
+            if (err) throw err;
+            res.render('configurator/administrator/fsUserEdit', {
+                user: result[0]
+            })
+
+        })
+    }
+    fsUserUpdate = (req, res) => {
+
+        /*   let data = {
+            _USER_LEGAL_NAME_: req.body.legalName,
+            USER_DEPARTEMENT: req.body.departement,
+            USER_ACTIVE: req.body.active,
+            USER_REASON: req.body.reason,
+            _USER_USERNAME: req.body.userName,
+            USER_TYPE: req.body.userType,
+            ___USER_PASSWORD: req.body.password
+        };
+ */
+
+
+        //const { id } = req.params
+        //const data = req.body
+        let sql = "UPDATE fs_user SET _USER_LEGAL_NAME_='" + req.body.legalName + "', USER_DEPARTEMENT='" + req.body.departement + "',  USER_ACTIVE='" + req.body.active + "', _USER_USERNAME='" + req.body.userName + "',  USER_REASON='" + req.body.reason + "',\
+         USER_TYPE='" + req.body.userType + "', ___USER_PASSWORD ='" + req.body.password + "' where USER_ID =" + req.body.id;
+        //let sql = 'UPDATE fs_user set ? where USER_ID = ?' + id
+        let query = connexion.query(sql, (err, results) => {
+            if (err) throw err;
+            res.redirect('/');
+        });
+
+    }
+
+
 }
 // Export controllers
 module.exports = userAdminControlleur
--- istreamExpress/backend/routers/fsUser.js
+++ istreamExpress/backend/routers/fsUser.js
@@ -6,8 +6,14 @@ const router = express.Router()
 const userAdminControlleur = require('../controllers/fsUserController')
 controlleur = new userAdminControlleur()
 
-router.get('/', (req, res) => controlleur.fsUserAdminDisplay(req, res))
-router.post('/save', (req, res) => controlleur.insertDataInFsuserTable(req, res))
+router.get('/', (req, res) => controlleur.fsUserList(req, res))
+    //post
+router.post('/save', (req, res) => controlleur.fsUserAdd(req, res))
+    //delete
+router.get('/delete/:id', (req, res) => controlleur.fsUserDelete(req, res))
+    //update
+router.get('/edit/:id', (req, res) => controlleur.fsUserEdit(req, res))
+router.post('/update', (req, res) => controlleur.fsUserUpdate(req, res))
 
 
 //Route export
